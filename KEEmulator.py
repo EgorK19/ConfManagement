c37d51fa9f7168d17995f8900ba048999af31e01
KEE <kochnevee73@gmail.con>
2025-09-20 11:30:46 +0300

    FirstStage

--- KEEmulator.py
+++ KEEmulator.py
@@ -1,79 +1,48 @@
 import os
 import sys
+import shlex
 
-def get_uh_name():
+def get_uhd():
     username = os.getlogin()
-    hostname = (
-        os.getenv('COMPUTERNAME', 'unknown')
-        if os.name == 'nt'
-        else getattr(os, 'uname', lambda: ('', 'unknown'))[1]
-    )
-    return f"{username}@{hostname}:~$ "
+    hostname = os.uname().nodename if hasattr(os, 'uname') else os.environ['COMPUTERNAME']
+    cwd = os.getcwd()
+    dir = os.path.expanduser('~')
+    if cwd.startswith(dir):
+        cwd = '~' + cwd[len(dir):]
+    return f"{username}@{hostname}:{cwd}$ "
 
 def parse_cmd(line):
     if not line.strip():
         return None, []
+    line = shlex.split(line)
+    cmd, args = line[0].lower(), line[1:]
+    return cmd," ".join(args)
 
-    parts = []
-    i = 0
-    n = len(line)
-    current_arg = ''
-    in_quotes = None
-
-    while i < n:
-        char = line[i]
-
-        if in_quotes is None:
-            if char.isspace():
-                if current_arg:
-                    parts.append(current_arg)
-                    current_arg = ''
-                i += 1
-                continue
-            elif char in '"\'':
-                in_quotes = char
-                i += 1
-                continue
-            else:
-                current_arg += char
-                i += 1
-        else:
-            if char == in_quotes:
-                parts.append(current_arg)
-                current_arg = ''
-                in_quotes = None
-                i += 1
-            else:
-                current_arg += char
-                i += 1
-
-    if current_arg:
-        parts.append(current_arg)
-
-    if not parts:
-        return None, []
-
-    return parts[0], parts[1:]
-
-def execute_cmd(cmd, args):
+def execute_cmd(cmd, args,t):
     if cmd == 'exit':
-        print("Exiting KEEmulator.")
+        print(f"{t}Exiting KEEmulator.")
         sys.exit(0)
     elif cmd == 'ls':
-        print(f"ls {' '.join(args)}")
+        print(f"ls {args}")
     elif cmd == 'cd':
-        print(f"cd {' '.join(args)}")
+        print(f"cd {args}")
     else:
-        print(f"shell: command not found: {cmd}")
+        print(f"{t}command not found: {cmd}")
 
 print("KEEmulator. Type 'exit' to quit.")
 while True:
+    t = get_uhd()
     try:
-        line = input(get_uh_name())
+        line = input(t)
         cmd, args = parse_cmd(line)
         if cmd:
-            execute_cmd(cmd, args)
+            execute_cmd(cmd, args,t)
     except KeyboardInterrupt:
-        print("\nUse 'exit' to quit.")
+        print("Type 'exit' to quit")
     except Exception as e:
-        print(f"shell: error: {e}")
+        print(f"{t} error: {e}")
+
+
+
+
+
